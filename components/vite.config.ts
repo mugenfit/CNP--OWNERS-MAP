4e4a9f534f14a1669603f7cbe362320f75718871
mugenfit <134262940+mugenfit@users.noreply.github.com>
2025-08-19 10:17:40 +0900

    Revert "feat: Introduce minimal component to isolate useState error"
    This reverts commit 184b59db4f3844ba996fc112a6489eb278156067.

--- components/vite.config.ts
+++ components/vite.config.ts
@@ -6,10 +6,7 @@ import { fileURLToPath } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CNP--OWNERS-MAP/',
-  build: { // Add this block
-    sourcemap: true,
-  }, // Add this line
+  base: '/CNP--OWNERS-MAP/', // Add this line
   resolve: {
     alias: {
       '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'src'),
